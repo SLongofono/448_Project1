efed322d1da7e4b891eb5b816b83cd0795f93177
Grant Jurgensen <gajurgensen@gmail.com>
2016-09-06 17:54:11 -0500

    basic Day class implemented

---
+++ Day.py
@@ -0,0 +1,23 @@
+class Day(object):
+
+	def __init__(self, month, weekday, date, details):
+		self.month = month
+		self.weekday = weekday
+		self.date = date
+		self.details = details
+		
+	def addDetails(self, detail):
+		self.details.append(detail)
+		return True #when would it be false?
+		
+	def editDetails(self, remove, idx)
+		if remove:
+			if idx < details.len():
+				del details[idx]
+				return True
+			else
+				return False
+				
+		else
+			#clarification on what to do?
+			
