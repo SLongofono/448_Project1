b65cae50551e8d963bb7b91497495eba16b5f801
Grant Jurgensen <gajurgensen@gmail.com>
2016-09-10 13:30:43 -0500

    added more tests to the main class

--- Main.py
+++ Main.py
@@ -2,5 +2,13 @@ from Year import Year
 from Day import Day
 
 testYear = Year(2016, 5)
-testDay = testYear.months[8].days[9]
+testMonth = testYear.months[8]
+testDay = testMonth.days[9]
+
 print (str(testDay.month) + "/" + str(testDay.date) + ", " + testDay.weekday)
+
+for week in testMonth.weeks:
+    for day in week:
+        print (str(day.month) + "/" + str(day.date) + ", " + day.weekday)
+
+    print ('\n')
