c0c017984842d62502b11e6a3f9cc7e8132e0979
Stephen Longofono <onofognol@ku.edu>
2016-09-16 22:14:58 -0500

    Added doxygen page for citations

---
+++ app/citations.py
@@ -0,0 +1,35 @@
+## @page Works Cited
+# @brief List of resources, code, and tutorials we used along the way
+# @author Stephen Longofono
+#
+# @section Citations
+#
+#
+# @subsection one Item 1
+#
+# Lorem Ipsum
+#
+# @subsection two Item 2
+#
+# Lorem Ipsum
+#
+# @subsection three Item 3
+#
+# Lorem Ipsum
+#
+# @subsection four Item 4
+#
+# Lorem Ipsum
+#
+# @subsection five Item 5
+#
+# Lorem Ipsum
+#
+# @subsection six Item 6
+#
+# Lorem Ipsum
+#
+# @subsection seven Item 7
+#
+# Lorem Ipsum
+#
