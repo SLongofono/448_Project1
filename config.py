4abc85edfd811808755dae36dc23e3e7405fb5cf
Stephen Wiss <stephenwiss@Stephens-MacBook-Pro.local>
2016-09-18 21:00:37 -0500

    merge

---
+++ config.py
@@ -0,0 +1,5 @@
+DEBUG = True
+WTF_CSRF_ENABLED = True         #Enable cross-site-request-forgery protection
+SECRET_KEY = 'grantstephensquared'  #Give our app a passkey to check against
+USERNAME = 'admin'
+PASSWORD = 'password'
