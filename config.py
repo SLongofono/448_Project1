11a8d6813904e8de9b04abec9d4b0deed0fcd198
Stephen Longofono <onofognol@ku.edu>
2016-09-18 22:42:44 -0500

    cleanup

--- config.py
+++
@@ -1,5 +0,0 @@
-DEBUG = True
-WTF_CSRF_ENABLED = True         #Enable cross-site-request-forgery protection
-SECRET_KEY = 'grantstephensquared'  #Give our app a passkey to check against
-USERNAME = 'admin'
-PASSWORD = 'password'
